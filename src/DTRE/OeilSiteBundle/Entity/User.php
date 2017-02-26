fce888e6547dd2b24a5a5ddd9e3eb9b4a05360c3
mathieuHa <mathieu.hanotaux@gmail.com>
2017-02-26 15:32:11 +0100

    add FOSUserBundle and set it inside template of the bundle

---
+++ src/DTRE/OeilSiteBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kafim
+ * Date: 26/02/2017
+ * Time: 12:19
+ */
+
+namespace DTRE\OeilSiteBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
